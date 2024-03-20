a61067f3b3b428f21bf9da13ff2953b9112ea1c6
Kentaro Nishioka <knishiok@c1r7s9.42tokyo.jp>
2024-03-21 02:51:34 +0900

    added base

--- srcs/img/intersect_cylinder.c
+++ srcs/img/intersect_cylinder.c
@@ -6,23 +6,49 @@
 /*   By: knishiok <knishiok@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:13:51 by knishiok          #+#    #+#             */
-/*   Updated: 2024/03/20 22:21:04 by knishiok         ###   ########.fr       */
+/*   Updated: 2024/03/21 02:50:04 by knishiok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "img.h"
 
-static bool	choose_dist(double k[2], double *dist)
+static bool	choose_dist(double k[2], t_intersection *res)
 {
 	if (k[0] < 0 && k[1] < 0)
 		return (false);
-	else if (k[0] > 0 && k[1] > 0)
-		*dist = fmin(k[0], k[1]);
-	else if (k[0] > 0)
-		*dist = k[0];
+	else if (k[0] < 0)
+	{
+		assert(k[1] >= 0);
+		if (k[1] < res->dist)
+		{
+			res->dist = k[1];
+			return (true);
+		}
+	}
+	else if (k[1] < 0)
+	{
+		assert(k[0] >= 0);
+		if (k[0] < res->dist)
+		{
+			res->dist = k[0];
+			return (true);
+		}
+	}
 	else
-		*dist = k[1];
-	return (true);
+	{
+		assert(k[0] >= 0 && k[1] >= 0);
+		if (k[0] < k[1] && k[0] < res->dist)
+		{
+			res->dist = k[0];
+			return (true);
+		}
+		else if (k[1] < k[0] && k[1] < res->dist)
+		{
+			res->dist = k[1];
+			return (true);
+		}
+	}
+	return (false);
 }
 
 static bool	valid_range(double dist, t_cylinder *cylinder)
@@ -57,6 +83,23 @@ static bool	has_intersection(double abc[3], double k[2],
 	return (true);
 }
 
+static void	hit_base(t_ray ray, t_cylinder *cylinder, t_intersection *res)
+{
+	t_plane			plane;
+	t_intersection	inter;
+	t_vector		upper;
+
+	plane = (t_plane){cylinder->origin, vector_mult(cylinder->normal, -1), cylinder->color};
+	inter = intersect_plane(ray, &plane);
+	if (norm(vector_sub(inter.coord, cylinder->origin)) <= cylinder->diameter)
+		*res = inter;
+	upper = vector_add(cylinder->origin, vector_mult(cylinder->normal, cylinder->height));
+	plane = (t_plane){upper, cylinder->normal, cylinder->color};
+	inter = intersect_plane(ray, &plane);
+	if (norm(vector_sub(inter.coord, upper)) <= cylinder->diameter && res->dist > inter.dist)
+		*res = inter;
+}
+
 t_intersection	intersect_cylinder(t_ray ray, t_cylinder *cylinder)
 {
 	double			abc[3];
@@ -64,8 +107,9 @@ t_intersection	intersect_cylinder(t_ray ray, t_cylinder *cylinder)
 	t_intersection	res;
 
 	res.dist = INF;
-	res.objptr = cylinder;
+	hit_base(ray, cylinder, &res);
 	res.type = CYLINDER;
+	res.objptr = cylinder;
 	abc[0] = pow(norm(cross(ray.direction, cylinder->normal)), 2.0);
 	if (abc[0] == 0)
 		return (res);
@@ -75,12 +119,12 @@ t_intersection	intersect_cylinder(t_ray ray, t_cylinder *cylinder)
 					cylinder->normal)), 2.0) - pow(cylinder->diameter, 2.0);
 	if (!has_intersection(abc, k, ray, cylinder))
 		return (res);
-	if (!choose_dist(k, &res.dist))
+	if (!choose_dist(k, &res))
 		return (res);
 	res.coord = vector_add(ray.origin, vector_mult(ray.direction, res.dist));
 	res.normal = normalize(vector_sub(vector_sub(res.coord, cylinder->origin),
 				vector_mult(cylinder->normal,
 					prod(vector_sub(res.coord, cylinder->origin),
-						cylinder->origin))));
+						cylinder->normal))));
 	return (res);
 }
